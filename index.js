f3647a972f2870dee9dea8706266e75f80ba3b68
ThisIsKhalid <iamkhalid.hs@gmail.com>
2022-10-25 01:03:17 +0600

    added courses data

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 5000;
+
+const courses = require('./data/courses.json');
+
+app.use(cors());
 
 
 app.get('/', (req, res) => {
     res.send('Server site is runnig')
 });
+
+app.get('/courses', (req, res) => {
+    res.send(courses);
+}) 
 
 app.listen(port, () => {
     console.log('Port is running on', port);
